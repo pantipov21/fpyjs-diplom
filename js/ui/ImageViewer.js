36093a33e267547cb015c110374b1ba6be085bb5
pantipov21 <94504528+pantipov21@users.noreply.github.com>
2022-10-17 14:59:50 +0300

    Add files via upload

--- js/ui/ImageViewer.js
+++ js/ui/ImageViewer.js
@@ -4,7 +4,18 @@
  * */
 class ImageViewer {
   constructor( element ) {
-
+	  this.imagesWrapper = element;
+	  this.previewImage = element.getElementsByClassName('column six wide')[0];
+	  
+	  this.imagesList = element.getElementsByClassName('row')[0];
+	this.btnSelectAll = this.imagesList.nextElementSibling.getElementsByClassName('select-all')[0];
+	this.btnSend = this.imagesList.nextElementSibling.getElementsByClassName('send')[0];
+	this.btnShowUploadedFiles = this.imagesList.nextElementSibling.getElementsByClassName('show-uploaded-files')[0];
+	  
+	  this.registerEvents();
+	  this.clickCount = 0;
+	  this.timeoutId = 0;
+	this.megaSelectionFlag = false;
   }
 
   /**
@@ -17,28 +28,153 @@ class ImageViewer {
    * 5. Клик по кнопке "Отправить на диск" открывает всплывающее окно для загрузки файлов
    */
   registerEvents(){
-
+	  
+		
+	  //пункт2.1
+	  this.imagesList.addEventListener('click',(event)=>{
+		  this.clickCount++;
+		  
+		  if (this.clickCount==1){
+			this.timeoutId = setTimeout(()=>{
+				if (this.clickCount == 1){//здесь обрабатывается одинарный клик
+					this.clickCount = 0;
+					clearTimeout(this.timeoutId);
+					  if (event.target.nodeName.toLowerCase()==='img'){
+						event.target.classList.toggle('selected');// здесь п.2.2
+						this.checkButtonText();
+					  }
+				}
+			},300);
+		  }
+		  
+		  
+		  if (this.clickCount >1){//здесь обрабатывается двойной клик
+			  clearTimeout(this.timeoutId);
+			  this.clickCount=0;
+			 
+			  if (event.target.nodeName.toLowerCase()==='img'){
+				  
+				while (this.previewImage.firstChild){
+					this.previewImage.removeChild(this.previewImage.firstChild);
+				}
+				
+				  const html = `
+				  <img class="ui fluid image" src=${event.target.src}>
+				  `;
+				  this.previewImage.insertAdjacentHTML('afterbegin', html);
+			  }
+		  }
+	  }, false);
+	  //конец п.2.1
+	  
+	  //пункт 2.3
+	  this.btnSelectAll.addEventListener('click', () => {
+		const images = Array.from(this.imagesList.getElementsByTagName('div'));
+		  
+  		if (this.megaSelectionFlag === false){
+			for (const elem of images){
+				elem.firstChild.classList.add('selected');
+			}			
+		}
+		else
+		{
+			for (const elem of images){
+				elem.firstChild.classList.remove('selected');
+			}			
+		}
+		  
+		this.checkButtonText();
+	  });
+	  
+	  //пункт 2.4
+	this.btnShowUploadedFiles.addEventListener('click', ()=>{
+		const images = Array.from(this.imagesList.getElementsByTagName('div'));
+		const fp = App.getModal('filePreviewer');
+		fp.elementDOM.children[1].children[0].style.display='block';
+		fp.open();
+		Yandex.getUploadedFiles((resp)=>{
+			if (resp.status==200){
+				const res = resp.response.items;
+				fp.showImages(res);
+				fp.elementDOM.children[1].children[0].style.display='none';
+			}
+		});
+	});
+	  //пункт 2.5
+	this.btnSend.addEventListener('click', ()=>{
+		const fu = App.getModal('fileUploader');
+		const images = Array.from(this.imagesList.getElementsByTagName('div'));
+		const imagesSelected = [];
+		for (const img of images){
+			if (img.firstChild.classList.contains('selected')){
+				imagesSelected.push(img.firstChild);
+			}
+		}
+		fu.open();
+		fu.showImages(imagesSelected);
+	});
   }
 
   /**
    * Очищает отрисованные изображения
    */
   clear() {
-
+	while (this.imagesList.firstChild){
+		this.imagesList.removeChild(this.imagesList.firstChild);
+	}
   }
 
   /**
    * Отрисовывает изображения.
   */
   drawImages(images) {
-
+	if (images.length > 0){
+		this.btnSelectAll.classList.remove('disabled');
+		let html=``;
+		images.forEach((item) => {
+			html = html+`
+			<div class='four wide column ui medium image-wrapper'><img src=${item} /></div>
+			`;
+		});
+		this.imagesList.insertAdjacentHTML('beforeend',html);
+	}
+	else
+	{
+		this.btnSelectAll.classList.add('disabled');
+	}
   }
 
   /**
-   * Контроллирует кнопки выделения всех изображений и отправки изображений на диск
+   * Контролирует кнопки выделения всех изображений и отправки изображений на диск
    */
   checkButtonText(){
-
+	const images = Array.from(this.imagesList.getElementsByTagName('div'));
+	let s = true;
+	let canSend = 0;  
+	for (const elem of images){
+		if (elem.firstChild.classList.contains('selected') == false){
+			s = false;
+			canSend++;
+		}
+		else
+		{
+			this.btnSend.classList.remove('disabled');
+		}
+	}
+	
+	if (canSend == images.length){
+		this.btnSend.classList.add('disabled');
+	}
+	
+	if (s===true){
+		this.btnSelectAll.innerText = 'снять выделение';
+		this.megaSelectionFlag = true;
+	}
+	else
+	{
+		this.btnSelectAll.innerText = 'выделить всё';
+		this.megaSelectionFlag = false;
+	}
   }
 
 }
--- js/ui/SearchBlock.js
+++ js/ui/SearchBlock.js
@@ -3,8 +3,10 @@
  * Используется для взаимодействием со строкой ввода и поиска изображений
  * */
 class SearchBlock {
+  
   constructor( element ) {
-
+	this.elementActual = element;
+	this.registerEvents();
   }
 
   /**
@@ -13,7 +15,30 @@ class SearchBlock {
    * только клик по кнопке "Заменить" перед отрисовкой очищает все отрисованные ранее изображения
    */
   registerEvents(){
-
+	const buttons = Array.from(document.getElementsByTagName('button'));
+	const btnReplace = buttons.find(item => item.classList.contains('replace'));
+	const btnAdd = buttons.find(item => item.classList.contains('add'));
+	  
+	const idInput = btnReplace.previousElementSibling;
+	  	
+	btnReplace.onclick = () => {
+		if (idInput.value.length>0){
+			VK.get(idInput.value,(data)=>{
+				App.imageViewer.clear();
+				App.imageViewer.drawImages(data);
+			});
+			return false;
+		}
+	}		
+	
+	btnAdd.onclick = () => {
+		if (idInput.value.length>0){
+			VK.get(idInput.value,(data)=>{
+				App.imageViewer.drawImages(data);
+			});
+			return false;
+		}
+	}
   }
 
 }
